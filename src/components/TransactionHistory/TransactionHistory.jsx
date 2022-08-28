01178f417b15dc65834f697ca7f372406b77cd0f
zhman1981 <zhman@ukr.net>
2022-08-28 23:03:10 +0300

    some change

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,32 +3,32 @@ import Transaction from 'components/Transaction/Transaction';
 import css from './TransactionHistory.module.css';
 
 const TransactionHistory = ({ items }) => {
-    return (
-        <table className={css.transaction_history}>
-            <thead>
-                <tr>
-                <th className={css.transaction_colum}>Type</th>
-                <th className={css.transaction_colum}>Amount</th>
-                <th className={css.transaction_colum}>Currency</th>
-                </tr>
-            </thead>
+  return (
+    <table className={css.transaction_history}>
+      <thead>
+        <tr>
+          <th className={css.transaction_colum}>Type</th>
+          <th className={css.transaction_colum}>Amount</th>
+          <th className={css.transaction_colum}>Currency</th>
+        </tr>
+      </thead>
 
-            <tbody>
-                {items.map(item => (
-                    <Transaction 
-                        key={item.id}
-                        type={item.type}
-                        amount={item.amount}
-                        currency={item.currency}
-                    />)
-                )}
-            </tbody>
-        </table>
-    );
+      <tbody>
+        {items.map(item => (
+          <Transaction
+            key={item.id}
+            type={item.type}
+            amount={item.amount}
+            currency={item.currency}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 TransactionHistory.propTypes = {
-    item: PropTypes.array,
-}
+  item: PropTypes.array,
+};
 
 export default TransactionHistory;
